be112a1fa6a334183bfb306292b39479e8610fd4
mrbai <745121731@qq.com>
2024-11-12 20:15:26 +0800

    修改时间

--- auto/auto/Tools/Tools.cpp
+++ auto/auto/Tools/Tools.cpp
@@ -343,6 +343,9 @@ int tool::ChinaStringHandle::Filter(char* chinastr, unsigned short* ll, int j)
 	return 0;
 }
 
+#include <iomanip>
+#include <sstream>
+#include <iostream>
 std::wstring tool::TimeTool::AddDay(int nDayAddNum)
 {
 	// 获取当前时间
@@ -362,7 +365,14 @@ std::wstring tool::TimeTool::AddDay(int nDayAddNum)
 		<< local_time->tm_min << ":"
 		<< local_time->tm_sec << std::endl;
 
+	std::wstringstream wssDate;
+	wssDate << std::setfill(L' ') << std::setw(4) << (local_time->tm_year + 1900)<< L"年"<< std::setfill(L' ') << std::setw(2) << (local_time->tm_mon + 1)<< L"月"<< std::setfill(L' ') << std::setw(2) << local_time->tm_mday << L"日";
+	std::wstring wszDate = wssDate.str();
+
+	return wszDate;
+	/*
 	wchar_t wszDate[MAX_PATH] = { 0 };
 	wsprintf(wszDate, L"%04d年%02d月%02d日", local_time->tm_year + 1900, local_time->tm_mon + 1, local_time->tm_mday);
+	*/
 	return wszDate;
 }
